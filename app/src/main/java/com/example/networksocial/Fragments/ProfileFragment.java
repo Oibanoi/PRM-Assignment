25faa99b004e83550cd3101ac124eedb4708a406
Huupb <76523587+Oibanoi@users.noreply.github.com>
2023-06-26 22:07:26 +0700

    add detail profile (#25)
    Co-authored-by: huu.pb <huu.pb@teko.vn>

--- app/src/main/java/com/example/networksocial/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/networksocial/Fragments/ProfileFragment.java
@@ -3,6 +3,7 @@ package com.example.networksocial.Fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -12,16 +13,30 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.networksocial.Activities.MainActivity;
 import com.example.networksocial.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
 
     FirebaseAuth firebaseAuth;
-
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    //views form xml
+    ImageView avatarTv;
+    TextView nameTv, emailTv, phoneTv;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -31,7 +46,52 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         firebaseAuth = FirebaseAuth.getInstance();
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //init firebase
+        user =firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+
+        //init views
+        avatarTv=view.findViewById(R.id.avatarTv);
+        nameTv=view.findViewById(R.id.nameTv);
+        emailTv=view.findViewById(R.id.emailTv);
+        phoneTv=view.findViewById(R.id.phoneTv);
+
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //check until require data get
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    //get data
+                    String name = ""+ds.child("name").getValue();
+                    String email = ""+ds.child("email").getValue();
+                    String phone = ""+ds.child("phone").getValue();
+                    String image = ""+ds.child("image").getValue();
+
+                    //set data
+                    nameTv.setText(name);
+                    emailTv.setText(email);
+                    phoneTv.setText(phone);
+                    try{
+                        Picasso.get().load(image).into(avatarTv);
+                    }catch (Exception e)
+                    {
+
+                        Picasso.get().load(R.drawable.ic_add_image).into(avatarTv);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
     }
 
     @Override
