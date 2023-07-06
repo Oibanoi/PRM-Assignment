e403c66f695a402fbf2726d929c6798314ff52b2
Trần Mạnh Hùng <96073507+HungHacker102S@users.noreply.github.com>
2023-07-06 23:00:13 +0700

    feat/style: Add Post (#34)

--- app/src/main/java/com/example/networksocial/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/networksocial/Fragments/ProfileFragment.java
@@ -32,6 +32,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.networksocial.Activities.AddPostActivity;
 import com.example.networksocial.Activities.MainActivity;
 import com.example.networksocial.R;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -418,7 +419,7 @@ public class ProfileFragment extends Fragment {
 
                 //picking from gallery, first check if storage permission allowed or not
                 if (grantResults.length > 0) {
-                    boolean writeStorageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                     if (writeStorageAccepted) {
                         //permission enabled
                         pickFromGallery();
@@ -593,6 +594,10 @@ public class ProfileFragment extends Fragment {
         if (id == R.id.action_logout) {
             firebaseAuth.signOut();
             checkUserStatus();
+        }
+
+        if(id == R.id.action_add_post) {
+            startActivity(new Intent(getActivity(), AddPostActivity.class));
         }
         return super.onOptionsItemSelected(item);
     }
